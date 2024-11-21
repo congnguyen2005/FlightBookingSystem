72afe337c3146a541e00f31167b2b15d72f593dc
daodu <dangcongnguyenst@gmail.com>
2024-11-21 09:05:06 +0700

    tim kiem chuyen bay

--- app/src/main/java/com/example/flightbookingsystem/MainActivity.java
+++ app/src/main/java/com/example/flightbookingsystem/MainActivity.java
@@ -8,21 +8,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.flightbookingsystem.Fragnment.ChooseFlightTicketFragment;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            ChooseFlightTicketFragment fragment = new ChooseFlightTicketFragment();
-
-            // Thêm Fragment vào container (ví dụ: một FrameLayout)
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.fragmentContainer, fragment)  // fragmentContainer là ID của container
-                    .commit();
-        }
     }
 }
